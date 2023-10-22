2562673a00ea86b5026b2ba80a5bbc9d435782e3
dtm589 <derekmarchese1@gmail.com>
2023-10-22 12:03:27 -0400

    add letter counting

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 with open("books/frankenstein.txt") as f:
     file_contents = f.read()
     # print(file_contents)
@@ -6,4 +8,12 @@ with open("books/frankenstein.txt") as f:
         words = file_contents.split()
         print(len(words))
 
+    def count_letters():
+        # conver to lower case
+        lower_case = file_contents.lower()
+        
+        print(dict(Counter(lower_case)))
+
+
     count_words()
+    count_letters()
